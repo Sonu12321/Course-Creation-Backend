b3ab9ce251e0aba7e45ec4b697fbbbfccd4be876
Sonu Santhosh <sonusanthosh371@gmail.com>
2025-05-18 18:53:15 +0530

    just something

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: process.env.CORS,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
